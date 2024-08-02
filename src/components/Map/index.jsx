e4b90a1ad357c4d68be3366e44211860a4c76161
Hemense Lan <hlan@afexnigeria.com>
2024-08-02 12:52:24 +0100

    google map key

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -36,7 +36,7 @@ export const LocationMap = () => {
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: import.meta.env.REACT_APP_GOOGLE_API_KEY }}
+        bootstrapURLKeys={{ key: import.meta.env.REACT_APP_GOOGLE_API_KEY ?? "" }}
         defaultCenter={center}
         defaultZoom={18}
         options={getMapOptions}>
